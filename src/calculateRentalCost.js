19524fc85c3fd7981da9d11be98fb30578585840
Bohdan Pasokha <bohdanbogoboom@Bohdans-MacBook-Pro.local>
2025-03-01 00:33:07 +0200

    task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let price = days * 40;
+
+  if (days >= 3 && days < 7) {
+    price -= 20;
+  }
+
+  if (days >= 7) {
+    price -= 50;
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
