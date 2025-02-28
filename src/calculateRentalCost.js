70857b9f0579b373ed59801c76e8e07e03ac4d9e
Bohdan Pasokha <bohdanbogoboom@Bohdans-MacBook-Pro.local>
2025-03-01 00:50:43 +0200

    task solution with fixed variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,17 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let price = days * 40;
+  const ONE_DAY_PRICE = 40;
+  const LOND_TERM = 7;
+  const SHORT_TERM = 3;
+  let PRICE_FOR_DAYS = days * ONE_DAY_PRICE;
 
-  if (days >= 3 && days < 7) {
-    price -= 20;
+  if (days >= SHORT_TERM && days < LOND_TERM) {
+    PRICE_FOR_DAYS -= 20;
   }
 
-  if (days >= 7) {
-    price -= 50;
+  if (days >= LOND_TERM) {
+    PRICE_FOR_DAYS -= 50;
   }
 
-  return price;
+  return PRICE_FOR_DAYS;
 }
 
 module.exports = calculateRentalCost;
